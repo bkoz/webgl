9ba56cdebcc89bfb777e537f9ccbef2a2060159b
Bob Kozdemba <bkozdemba@gmail.com>
2023-03-18 22:06:41 -0500

    fixed

--- project1.js
+++ project1.js
@@ -1,23 +1,21 @@
+//
+// Composite (alpha blend) the foreground image with the background image.
+//
 // bgImg is the background image to be modified.
 // fgImg is the foreground image.
 // fgOpac is the opacity of the foreground image.
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
+//
+// Bob Kozdemba - bkozdemba@gmail.com
+//
+// float fgOpac - From UI.
+// 
 function composite(bgImg, fgImg, fgOpac, fgPos) {
         for (var i = 0; i < bgImg.data.length; i += 4) {
-                bgImg.data[i + 0] = fgOpac * fgImg.data[i + 0] + (1 - fgOpac) * bgImg.data[i + 0];
-                bgImg.data[i + 1] = fgOpac * fgImg.data[i + 1] + (1 - fgOpac) * bgImg.data[i + 1];
-                bgImg.data[i + 2] = fgOpac * fgImg.data[i + 2] + (1 - fgOpac) * bgImg.data[i + 2];
-                bgImg.data[i + 3] = fgOpac * fgImg.data[i + 3] + (1 - fgOpac) * bgImg.data[i + 3];
+                mask = fgImg.data[i + 3] * fgOpac;
+                bgImg.data[i + 0] = mask / 255 * fgImg.data[i + 0] + (1 - mask / 255) * bgImg.data[i + 0];
+                bgImg.data[i + 1] = mask / 255 * fgImg.data[i + 1] + (1 - mask / 255) * bgImg.data[i + 1];
+                bgImg.data[i + 2] = mask / 255 * fgImg.data[i + 2] + (1 - mask / 255) * bgImg.data[i + 2];
+                bgImg.data[i + 3] = mask / 255 * fgImg.data[i + 3] + (1 - mask / 255) * bgImg.data[i + 3];
         }
-
-}
-
-function new-composite(bgImg, fgImg, fgOpac, fgPos) {
-        for (var i = 0; i < bgImg.data.length; i += 4) {
-                mask = fgImg.data[i+3];
-                bgImg.data[i + 0] = mask / 255 * fgImg.data[i+0] + (1 - mask / 255 ) * bgImg.data[i+0];
-                bgImg.data[i + 1] = mask / 255 * fgImg.data[i+1] + (1 - mask / 255 ) * bgImg.data[i+1];
-                bgImg.data[i + 2] = mask / 255 * fgImg.data[i+2] + (1 - mask / 255 ) * bgImg.data[i+2];
-        }
-
 }
